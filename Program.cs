7649a09d3a76f58eaaa092d2c1f1448549de2190
alvinyanson <a.yanson@arcanys.com>
2024-08-09 13:47:49 +0800

    implemented token bucket limiter

--- Program.cs
+++ Program.cs
@@ -1,21 +1,23 @@
 using Microsoft.AspNetCore.RateLimiting;
 using RateLimiting;
 using System.Threading.RateLimiting;
+using WebRateLimitAuth.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var tokenPolicy = "token";
 var myOptions = new MyRateLimitOptions();
 builder.Configuration.GetSection(MyRateLimitOptions.MyRateLimit).Bind(myOptions);
-var slidingPolicy = "sliding";
 
 builder.Services.AddRateLimiter(_ => _
-    .AddSlidingWindowLimiter(policyName: slidingPolicy, options =>
+    .AddTokenBucketLimiter(policyName: tokenPolicy, options =>
     {
-        options.PermitLimit = myOptions.PermitLimit;
-        options.Window = TimeSpan.FromSeconds(myOptions.Window);
-        options.SegmentsPerWindow = myOptions.SegmentsPerWindow;
+        options.TokenLimit = myOptions.TokenLimit;
         options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
         options.QueueLimit = myOptions.QueueLimit;
+        options.ReplenishmentPeriod = TimeSpan.FromSeconds(myOptions.ReplenishmentPeriod);
+        options.TokensPerPeriod = myOptions.TokensPerPeriod;
+        options.AutoReplenishment = myOptions.AutoReplenishment;
     }));
 
 var app = builder.Build();
@@ -24,7 +26,7 @@ app.UseRateLimiter();
 
 static string GetTicks() => (DateTime.Now.Ticks & 0x11111).ToString("00000");
 
-app.MapGet("/", () => Results.Ok($"Sliding Window Limiter {GetTicks()}"))
-                           .RequireRateLimiting(slidingPolicy);
+app.MapGet("/", () => Results.Ok($"Token Limiter {GetTicks()}"))
+                           .RequireRateLimiting(tokenPolicy);
 
 app.Run();
